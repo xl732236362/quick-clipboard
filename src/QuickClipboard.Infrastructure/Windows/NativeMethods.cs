bb8124265c14cc415212b66cf77427c66046f112
xl732236362 <732236362@qq.com>
2026-06-23 14:50:44 +0800

    feat: add global hotkey service

--- src/QuickClipboard.Infrastructure/Windows/NativeMethods.cs
+++ src/QuickClipboard.Infrastructure/Windows/NativeMethods.cs
@@ -5,6 +5,7 @@ namespace QuickClipboard.Infrastructure.Windows;
 internal static partial class NativeMethods
 {
     internal const int WM_CLIPBOARDUPDATE = 0x031D;
+    internal const int WM_HOTKEY = 0x0312;
 
     [LibraryImport("user32.dll", SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
@@ -13,4 +14,12 @@ internal static partial class NativeMethods
     [LibraryImport("user32.dll", SetLastError = true)]
     [return: MarshalAs(UnmanagedType.Bool)]
     internal static partial bool RemoveClipboardFormatListener(IntPtr hwnd);
+
+    [LibraryImport("user32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    internal static partial bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+    [LibraryImport("user32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    internal static partial bool UnregisterHotKey(IntPtr hWnd, int id);
 }
